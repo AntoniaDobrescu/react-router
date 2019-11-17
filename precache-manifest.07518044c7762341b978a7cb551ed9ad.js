e77826fb059a1ad05ab64948036f276b6b52c432
AntoniaDobrescu <antonia.dobrescu91@gmail.com>
2019-11-17 15:37:28 +0200

    Updates

---
+++ precache-manifest.07518044c7762341b978a7cb551ed9ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed0aa16f9aacc0b962701a474e5f38e",
+    "url": "/react-router/index.html"
+  },
+  {
+    "revision": "eab8191ba9829797ef91",
+    "url": "/react-router/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "64b63b6da9d7fcd5744c",
+    "url": "/react-router/static/js/2.1b1fcffa.chunk.js"
+  },
+  {
+    "revision": "eab8191ba9829797ef91",
+    "url": "/react-router/static/js/main.1cda0211.chunk.js"
+  },
+  {
+    "revision": "7f565af7659f1291b8e3",
+    "url": "/react-router/static/js/runtime-main.b7b70acb.js"
+  }
+]);
